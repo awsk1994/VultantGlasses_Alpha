36f7c139d61f8f28c5563ce5822adf9f3750441e
Alex Wong <awsk1994@gmail.com>
2021-01-27 21:07:07 +0800

    Test file for str/hex to unicode.

---
+++ unicodeTest.js
@@ -0,0 +1,26 @@
+function strToUnicode(str, toUnicodeString=false) {
+	return str.split('').map(function (value, index, array) {
+    var temp = value.charCodeAt(0).toString(16).padStart(4, '0'); // converts it to hex string
+		return toUnicodeString ? '\\u' + temp : String.fromCharCode(parseInt(temp, 16));
+	}).join('');
+};
+
+function singleHexToUnicode(str, toHexString) {
+  return toHexString ? 'x' + str : String.fromCharCode(parseInt(str, 16));
+}
+
+console.log("===== strToUnicode =====");
+console.log(strToUnicode('你好'));
+console.log(strToUnicode('你好', toUnicodeString=true));  // \u4f60\u597d 
+console.log(strToUnicode('abc'));
+console.log(strToUnicode('abc', toUnicodeString=true));   // \u0061\u0062\u0063
+
+console.log("\n\n===== singleHexToUnicode =====");
+console.log(singleHexToUnicode('61'));  // a
+console.log(singleHexToUnicode('61', toHexString=true)); // x61
+console.log(singleHexToUnicode('100')); // Ā
+console.log(singleHexToUnicode('400')); // Ѐ
+console.log(singleHexToUnicode('A0'));
+
+console.log(singleHexToUnicode('100') + singleHexToUnicode('400')); // ĀЀ
+
