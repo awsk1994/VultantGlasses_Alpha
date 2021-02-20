74b5d15151b21deaf384531a7f3b3aa982d31156
Alex Wong <awsk1994@gmail.com>
2021-02-20 19:29:53 +0800

    Update Storage library.

--- components/Storage.js
+++ components/Storage.js
@@ -12,10 +12,32 @@ class Storage {
     }
   }
 
+  static fetchBool = async (key) => {
+    console.log("Fetching text with key(" + key + ")");
+    try {
+      const value = await AsyncStorage.getItem(key);
+      console.log("Fetch complete.");
+      return value == "true";
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   static saveText = async (key, value) => {
     console.log("Saving Text with key(" + key + ")");
     try {
       await AsyncStorage.setItem(key, value);
+      console.log("save Complete.");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  static saveBool = async (key, value) => {
+    console.log("Saving Bool with key(" + key + ")");
+    try {
+      const strVal = value ? "true": "false";
+      await AsyncStorage.setItem(key, strVal);
       console.log("save Complete.");
     } catch (e) {
       console.log(e);
