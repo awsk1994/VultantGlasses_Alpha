9be89e2bd74e74e9041e3f80b4f9f9acd4c20693
Alex Wong <awsk1994@gmail.com>
2021-09-13 00:51:01 +0800

    Add QQ to notification list

--- src/data/InitAllowAppList.js
+++ src/data/InitAllowAppList.js
@@ -4,5 +4,6 @@ export default [
   "com.facebook.orca", 
   "com.google.android.talk",  // Hangout
   "com.google.android.calendar", 
-  "com.tencent.mm"  // WeChat
+  "com.tencent.mm",  // WeChat
+  "com.tencent.mobileqq" // QQ
 ];
