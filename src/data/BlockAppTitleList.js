3374bf1d608ff4455d24ffe609f089fe83562b57
Alex Wong <awsk1994@gmail.com>
2021-09-13 00:51:21 +0800

    Remove block kingmelon google hangout msg

--- src/data/BlockAppTitleList.js
+++ src/data/BlockAppTitleList.js
@@ -3,5 +3,5 @@ export default {
   "com.whatsapp": ["WhatsApp", "WhatsApp Web"],
   "com.facebook.orca": ["Chat heads active"],
   "android": ["Messenger is displaying over other apps"],
-  "com.google.android.talk": ["king"],
+  "com.google.android.talk": [],
 };
